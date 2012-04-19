46e289bfa0e04a5830aca866b062bc99ec7b862f
Tim Crowe <tim.crowe@timmyfury.com>
2012-04-19 14:10:52 -0400

    competency filtering by skill

--- behavior.js
+++ behavior.js
@@ -42,27 +42,75 @@
 	**********************************************************************/
 	$(function(){
 
-		// var filters = $("#skills .filter");
-		// 
-		// filters.click(function(evt){
-		// 	
-		// 	var target = $(this),
-		// 		filters = target.data('filters').split(' '),
-		// 		hideFilters = _.map(filters, function(f){ return "." + f; }),
-		// 		hideSelector = ".competency:not(" + hideFilters.join(",") + ")",
-		// 		showFilters = _.map(filters, function(f){ return ".competency." + f; }),
-		// 		showSelector = showFilters.join(","),
-		// 
-		// 		experiences = $(".experience"),
-		// 		projects = $(".project");
-		// 	
-		// 	$(hideSelector).hide();
-		// 	$(showSelector).show();
-		// 
-		// 	experiences.hide().has(showSelector).show();
-		// 	projects.hide().has(showSelector).show();
-		// 
-		// });
+		var addCompetencyFilter = function(target, filters, activeFilters){
+				target.addClass('active-filter');
+				var _activeFilters = _.flatten([activeFilters, filters]);
+				return filterCompetencies(_activeFilters);
+			},
+
+			removeCompetencyFilter = function(target, filters, activeFilters){
+				target.removeClass('active-filter');
+				var _activeFilters = _.filter(activeFilters, function(f){ return _.indexOf(filters, f) == -1; });
+				return filterCompetencies(_activeFilters);
+			},
+
+			filterCompetencies = function(activeFilters){
+				console.log(activeFilters);
+
+				var skillsListNode = $('#skills ul'),
+					hideFilters = _.map(activeFilters, function(f){ return "." + f; }),
+					hideSelector = ".competency:not(" + hideFilters.join(",") + ")",
+					showFilters = _.map(activeFilters, function(f){ return ".competency." + f; }),
+					showSelector = showFilters.join(","),
+					experiences = $(".experience"),
+					projects = $(".project"),
+					sections = $('#work, #play');
+
+				if(activeFilters.length){
+					skillsListNode.addClass('filtering');
+					$(hideSelector).hide();
+					$(showSelector).show();
+
+					experiences.hide().has(showSelector).show();
+					projects.hide().has(showSelector).show();
+					sections.hide().has(showSelector).show();
+
+					clearNode.show();
+				} else {
+					skillsListNode.removeClass('filtering');
+					$('.competency').show();
+					experiences.show();
+					projects.show();
+					sections.show();
+					clearNode.hide();
+				}
+
+				return activeFilters;
+			};
+
+		var skillsNode = $('#skills'),
+			filterNodes = skillsNode.find(".filter"),
+			activeFilters = [],
+			instructionsNode = $('<p>Select skills below to filter competencies by. <a href="#" style="display: none;">Clear all filters.</a></p>'),
+			clearNode = instructionsNode.find('a');
+
+		skillsNode.find('h2').after(instructionsNode);
+
+		clearNode.click(function(evt){
+			evt.preventDefault();
+			activeFilters = removeCompetencyFilter(filterNodes, activeFilters, activeFilters);
+		});
+
+		filterNodes.click(function(evt){
+			var target = $(this),
+				filters = target.data('filters').split(' ');
+
+			if(target.hasClass('active-filter')){
+				activeFilters = removeCompetencyFilter(target, filters, activeFilters);
+			} else {
+				activeFilters = addCompetencyFilter(target, filters, activeFilters);
+			}
+		});
 
 	});
 }(jQuery));
